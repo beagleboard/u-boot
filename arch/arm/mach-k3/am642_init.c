beagleboard/u-boot

commit e130468298883a7fed0c55e627d9c726ff25d699

arm: mach-k3: am642_init: Do USB fixups to facilitate host and device boot modes
U-Boot either supports USB host or device mode for a node at a time in the
device tree nodes. To support both host and dfu bootmodes, dr_mode is set
to "peripheral" by default and then fixed based on the mode selected by
the boot mode config dip switches on the board.

This needs to happen before the cdns3 generic layer binds the usb device
to a host or a device driver. Therefore, use fdtdec_setup_board()
implementation to fixup the device tree property.

Signed-off-by: Aswath Govindraju <a-govindraju@ti.com>

arch/arm/mach-k3/am642_init.c
@@ -8,6 +8,7 @@
  */
 
 #include <common.h>
+#include <fdt_support.h>
 #include <spl.h>
 #include <asm/io.h>
 #include <asm/arch/hardware.h>
@@ -80,6 +81,38 @@ void k3_mmc_restart_clock(void)
 #else
 void k3_mmc_stop_clock(void) {}
 void k3_mmc_restart_clock(void) {}
+#endif
+
+#if CONFIG_IS_ENABLED(USB_STORAGE)
+static int fixup_usb_boot(const void *fdt_blob)
+{
+	int ret = 0;
+
+	switch (spl_boot_device()) {
+	case BOOT_DEVICE_USB:
+		/*
+		 * If the boot mode is host, fixup the dr_mode to host
+		 * before cdns3 bind takes place
+		 */
+		ret = fdt_find_and_setprop((void *)fdt_blob,
+					   "/bus@f4000/cdns-usb@f900000/usb@f400000",
+					   "dr_mode", "host", 11, 0);
+		if (ret)
+			printf("%s: fdt_find_and_setprop() failed:%d\n",
+			       __func__, ret);
+		fallthrough;
+	default:
+		break;
+	}
+
+	return ret;
+}
+
+int fdtdec_board_setup(const void *fdt_blob)
+{
+	/* Can use the pointer from the function parameters */
+	return fixup_usb_boot(fdt_blob);
+}
 #endif
 
 void board_init_f(ulong dummy)

commit d0d92256cbd897062c48436aae1e6c9dfc5dcf88

arm: mach-k3: am642_init: Fix the length of new property value in fdt_find_and_setprop()
In function fdt_find_and_setprop(), argument "len" denotes the length of
the new property value only, not accumulated sum of lengths of property
name and property value. Therefore, fix it to the correct length of string
"host", which is 4.

Fixes: e13046829888 ("arm: mach-k3: am642_init: Do USB fixups to facilitate host and device boot modes")
Signed-off-by: Aswath Govindraju <a-govindraju@ti.com>

arch/arm/mach-k3/am642_init.c
@@ -96,7 +96,7 @@ static int fixup_usb_boot(const void *fdt_blob)
 		 */
 		ret = fdt_find_and_setprop((void *)fdt_blob,
 					   "/bus@f4000/cdns-usb@f900000/usb@f400000",
-					   "dr_mode", "host", 11, 0);
+					   "dr_mode", "host", 4, 0);
 		if (ret)
 			printf("%s: fdt_find_and_setprop() failed:%d\n",
 			       __func__, ret);