63befb6f2e90b152bf9cf99c58d933ce7cf68276
Paul Barker <paul.barker@sancloud.com>
2021-05-19 19:06:43 +0100

    board: ti: am335x: Add BBE Lite support
    The Sancloud BeagleBone Enhanced Lite (BBE Lite) has its own devicetree
    file and the board can be identified by the 2nd letter of the config
    string within the common EEPROM.
    
    Signed-off-by: Paul Barker <paul.barker@sancloud.com>
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- include/configs/am335x_evm.h
+++ include/configs/am335x_evm.h
@@ -215,6 +215,8 @@
 			"setenv fdtfile am335x-boneblue.dtb; fi; " \
 		"if test $board_name = BBEN; then " \
 			"setenv fdtfile am335x-sancloud-bbe.dtb; fi; " \
+		"if test $board_name = BBELITE; then " \
+			"setenv fdtfile am335x-sancloud-bbe-lite.dtb; fi; " \
 		"if test $board_name = OS00; then " \
 			"setenv fdtfile am335x-osd3358-sm-red.dtb; fi; " \
 		"if test $board_name = A33515BB; then " \
