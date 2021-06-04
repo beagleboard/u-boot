979abcd879251cc65796cb7ee853cc12849d09c9
Aswath Govindraju <a-govindraju@ti.com>
2021-06-04 22:43:00 +0530

    configs: am64: Add and enable configs required for USB DFU boot mode support
    Add and enable configs required to add support for USB DFU boot mode.
    
    Signed-off-by: Aswath Govindraju <a-govindraju@ti.com>

--- include/configs/am64x_evm.h
+++ include/configs/am64x_evm.h
@@ -13,6 +13,7 @@
 #include <config_distro_bootcmd.h>
 #include <environment/ti/mmc.h>
 #include <asm/arch/am64_hardware.h>
+#include <environment/ti/k3_dfu.h>
 
 /* DDR Configuration */
 #define CONFIG_SYS_SDRAM_BASE1		0x880000000
@@ -104,13 +105,20 @@
 #endif
 
 
+#define EXTRA_ENV_DFUARGS \
+	DFU_ALT_INFO_MMC \
+	DFU_ALT_INFO_EMMC \
+	DFU_ALT_INFO_RAM \
+	DFU_ALT_INFO_OSPI
+
 /* Incorporate settings into the U-Boot environment */
 #define CONFIG_EXTRA_ENV_SETTINGS					\
 	DEFAULT_LINUX_BOOT_ENV						\
 	DEFAULT_MMC_TI_ARGS						\
 	EXTRA_ENV_AM642_BOARD_SETTINGS					\
 	EXTRA_ENV_AM642_BOARD_SETTINGS_MMC				\
-	EXTRA_ENV_AM642_BOARD_SETTINGS_MTD
+	EXTRA_ENV_AM642_BOARD_SETTINGS_MTD				\
+	EXTRA_ENV_DFUARGS
 
 /* Now for the remaining common defines */
 #include <configs/ti_armv7_common.h>
