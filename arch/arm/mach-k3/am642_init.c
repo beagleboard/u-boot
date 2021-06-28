beagleboard/u-boot

commit d0d92256cbd897062c48436aae1e6c9dfc5dcf88

arm: mach-k3: am642_init: Fix the length of new property value in fdt_find_and_setprop()
In function fdt_find_and_setprop(), argument "len" denotes the length of
the new property value only, not accumulated sum of lengths of property
name and property value. Therefore, fix it to the correct length of string
"host", which is 4.

Fixes: e13046829888 ("arm: mach-k3: am642_init: Do USB fixups to facilitate host and device boot modes")
Signed-off-by: Aswath Govindraju <a-govindraju@ti.com>

arch/arm/mach-k3/am642_init.c
@@ -96,7 +96,7 @@ static int fixup_usb_boot(const void *fdt_blob)
 		 */
 		ret = fdt_find_and_setprop((void *)fdt_blob,
 					   "/bus@f4000/cdns-usb@f900000/usb@f400000",
-					   "dr_mode", "host", 11, 0);
+					   "dr_mode", "host", 4, 0);
 		if (ret)
 			printf("%s: fdt_find_and_setprop() failed:%d\n",
 			       __func__, ret);

commit c0abb51a288b47b0d6b9a727ff7c9982f47dd9e6

arm: mach-k3: am642_init: Correct the length of the host string to 5 instead of 4
While calculating the length of the property value to be replaced with,
also include the "\0" character. This makes the length of the new property
value "host" to be 5 and not 4.

Fixes: d0d92256cbd8 ("arm: mach-k3: am642_init: Fix the length of new property value in fdt_find_and_setprop()")
Signed-off-by: Aswath Govindraju <a-govindraju@ti.com>

arch/arm/mach-k3/am642_init.c
@@ -97,7 +97,7 @@ static int fixup_usb_boot(const void *fdt_blob)
 		 */
 		ret = fdt_find_and_setprop((void *)fdt_blob,
 					   "/bus@f4000/cdns-usb@f900000/usb@f400000",
-					   "dr_mode", "host", 4, 0);
+					   "dr_mode", "host", 5, 0);
 		if (ret)
 			printf("%s: fdt_find_and_setprop() failed:%d\n",
 			       __func__, ret);