beagleboard/u-boot

commit d7ca60b8754103005386d1398cb04f28bd61dbc2

ram: k3-ddrss: Allow use of dt provided initial frequency
Allow device tree to provide ti,ddr-freq0 to be used as the initial DDR
frequency that is set for lpddr4 before initialization of the
controller. Make this optional and continue to use bypass frequency if
ti,ddr-freq0 is not provided.

Signed-off-by: Dave Gerlach <d-gerlach@ti.com>
[praneeth@ti.com: fix minor build error]
Signed-off-by: Praneeth Bajjuri <praneeth@ti.com>

drivers/ram/k3-ddrss/k3-ddrss.c
@@ -35,6 +35,7 @@ struct k3_ddrss_desc {
 	struct power_domain ddrdata_pwrdmn;
 	struct clk ddr_clk;
 	struct clk osc_clk;
+	u32 ddr_freq0;
 	u32 ddr_freq1;
 	u32 ddr_freq2;
 	u32 ddr_fhs_cnt;
@@ -124,9 +125,7 @@ static void k3_lpddr4_freq_update(void)
 		else if (req_type == 2)
 			clk_set_rate(&ddrss->ddr_clk, ddrss->ddr_freq2);
 		else if (req_type == 0)
-			/* Put DDR pll in bypass mode */
-			clk_set_rate(&ddrss->ddr_clk,
-				     clk_get_rate(&ddrss->osc_clk));
+			clk_set_rate(&ddrss->ddr_clk, ddrss->ddr_freq0);
 		else
 			printf("%s: Invalid freq request type\n", __func__);
 
@@ -175,8 +174,7 @@ static int k3_ddrss_init_freq(struct k3_ddrss_desc *ddrss)
 		ret = clk_set_rate(&ddrss->ddr_clk, ddrss->ddr_freq1);
 		break;
 	case DENALI_CTL_0_DRAM_CLASS_LPDDR4:
-		/* Set to bypass frequency for LPDDR4*/
-		ret = clk_set_rate(&ddrss->ddr_clk, clk_get_rate(&ddrss->osc_clk));
+		ret = clk_set_rate(&ddrss->ddr_clk, ddrss->ddr_freq0);
 		break;
 	default:
 		ret = -EINVAL;
@@ -273,6 +271,14 @@ static int k3_ddrss_ofdata_to_priv(struct udevice *dev)
 	ret = clk_get_by_index(dev, 1, &ddrss->osc_clk);
 	if (ret)
 		dev_err(dev, "clk get failed for osc clk %d\n", ret);
+
+	ret = dev_read_u32(dev, "ti,ddr-freq0", &ddrss->ddr_freq0);
+	if (ret) {
+		ddrss->ddr_freq0 = clk_get_rate(&ddrss->osc_clk);
+		dev_dbg(dev,
+			"ddr freq0 not populated, using bypass frequency.\n",
+			ret);
+	}
 
 	ret = dev_read_u32(dev, "ti,ddr-freq1", &ddrss->ddr_freq1);
 	if (ret)

commit 4bf1c4b9ad1eb960ad86307b253e56c2b7eacce4

ram: k3-ddrss: Fix a dev_dbg build warning
The k3-ddrss RAM driver generates the following build warning due to a
missing printk argument specifier in a dev_dbg. Fix this by printing
the actual bypass frequency.

drivers/ram/k3-ddrss/k3-ddrss.c:279:4: warning: too many arguments for format [-Wformat-extra-args]
  279 |    "ddr freq0 not populated, using bypass frequency.\n",
      |    ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

Fixes: d7ca60b875410 ("ram: k3-ddrss: Allow use of dt provided initial frequency")
Signed-off-by: Suman Anna <s-anna@ti.com>

drivers/ram/k3-ddrss/k3-ddrss.c
@@ -276,8 +276,8 @@ static int k3_ddrss_ofdata_to_priv(struct udevice *dev)
 	if (ret) {
 		ddrss->ddr_freq0 = clk_get_rate(&ddrss->osc_clk);
 		dev_dbg(dev,
-			"ddr freq0 not populated, using bypass frequency.\n",
-			ret);
+			"ddr freq0 not populated, using bypass frequency %u\n",
+			ddrss->ddr_freq0);
 	}
 
 	ret = dev_read_u32(dev, "ti,ddr-freq1", &ddrss->ddr_freq1);