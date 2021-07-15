c5348bd884f29048fbf5e7b9fe55a5a38c1dc610
Gowtham Tammana <g-tammana@ti.com>
2021-07-14 20:24:21 -0500

    j721e_init: initialize ESM support for J7200 SOM
    ESM and ESM PMIC support is available for J7200X-PM2-SOM board, as such
    enable support for it.
    
    Signed-off-by: Gowtham Tammana <g-tammana@ti.com>

--- arch/arm/mach-k3/j721e_init.c
+++ arch/arm/mach-k3/j721e_init.c
@@ -303,7 +303,8 @@ void board_init_f(ulong dummy)
 #endif
 
 #ifdef CONFIG_ESM_K3
-	if (board_ti_k3_is("J721EX-PM2-SOM")) {
+	if (board_ti_k3_is("J721EX-PM2-SOM") ||
+	    board_ti_k3_is("J7200X-PM2-SOM")) {
 		ret = uclass_get_device_by_driver(UCLASS_MISC,
 						  DM_GET_DRIVER(k3_esm), &dev);
 		if (ret)
@@ -312,7 +313,8 @@ void board_init_f(ulong dummy)
 #endif
 
 #ifdef CONFIG_ESM_PMIC
-	if (board_ti_k3_is("J721EX-PM2-SOM")) {
+	if (board_ti_k3_is("J721EX-PM2-SOM") ||
+	    board_ti_k3_is("J7200X-PM2-SOM")) {
 		ret = uclass_get_device_by_driver(UCLASS_MISC,
 						  DM_GET_DRIVER(pmic_esm),
 						  &dev);
