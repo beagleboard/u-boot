1f71ed325681a83c8d7a1c097b519cac7555bd5f
Robert Nelson <robertcnelson@gmail.com>
2022-01-31 10:26:52 -0600

    add simple build/install scirpts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ build-am335x.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+sys=$(uname -m)
+
+#add: bc bison flex libssl-dev u-boot-tools binutils gcc
+
+DIR="$PWD"
+
+if [ "x${sys}" = "xarmv7l" ] ; then
+	if [ ! -f ./load.menuconfig ] ; then
+		echo "Developers: too make changes: [touch load.menuconfig]"
+	fi
+fi
+
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
+
+if [ "x${sys}" = "xarmv7l" ] ; then
+	if [ -f ./load.menuconfig ] ; then
+		make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- am335x_evm_defconfig
+		make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
+		make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- savedefconfig
+		cp -v defconfig ./configs/am335x_evm_defconfig
+		make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
+	fi
+
+	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- am335x_evm_defconfig
+	make ARCH=arm -j2 CROSS_COMPILE=arm-linux-gnueabihf-
+else
+	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- am335x_evm_defconfig
+	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
+	make ARCH=arm -j2 CROSS_COMPILE=arm-linux-gnueabihf-
+
+	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- savedefconfig
+	cp -v defconfig ./configs/am335x_evm_defconfig
+fi
