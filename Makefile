beagleboard/u-boot

commit dd0bb6313c97ff8b2e9ec135125976e353f007aa

binman: Add support for TEE BL32
Add an entry for OP-TEE Trusted OS 'BL32' payload.
This is required by platforms using Cortex-A cores with TrustZone
technology.

Signed-off-by: Roger Quadros <rogerq@kernel.org>

Makefile
@@ -1335,6 +1335,7 @@ cmd_binman = $(srctree)/tools/binman/binman $(if $(BINMAN_DEBUG),-D) \
 		-I . -I $(srctree) -I $(srctree)/board/$(BOARDDIR) \
 		-I arch/$(ARCH)/dts -a of-list=$(CONFIG_OF_LIST) \
 		-a atf-bl31-path=${BL31} \
+		-a atf-bl32-path=${BL32} \
 		-a default-dt=$(default_dt) \
 		-a scp-path=$(SCP) \
 		$(BINMAN_$(@F))

tools/binman/README.entries
@@ -25,6 +25,19 @@ about ATF.
 
 
 
+Entry: atf-bl32: Entry containing an OP-TEE Trusted OS (TEE) BL32 blob
+-------------------------------------------------------------------------
+
+Properties / Entry arguments:
+    - atf-bl32-path: Filename of file to read into entry. This is typically
+        called bl32.bin or bl32.elf
+
+This entry holds the run-time firmware, typically started by U-Boot SPL.
+See the U-Boot README for your architecture or board for how to use it. See
+https://github.com/OP-TEE/optee_os for more information about OP-TEE.
+
+
+
 Entry: blob: Entry containing an arbitrary binary blob
 ------------------------------------------------------
 

tools/binman/ftest.py
@@ -76,6 +76,7 @@ FSP_M_DATA            = b'fsp_m'
 FSP_S_DATA            = b'fsp_s'
 FSP_T_DATA            = b'fsp_t'
 ATF_BL31_DATA         = b'bl31'
+ATF_BL32_DATA         = b'bl32'
 SCP_DATA              = b'scp'
 TEST_FDT1_DATA        = b'fdt1'
 TEST_FDT2_DATA        = b'test-fdt2'
@@ -178,6 +179,7 @@ class TestFunctional(unittest.TestCase):
         TestFunctional._MakeInputFile('compress', COMPRESS_DATA)
         TestFunctional._MakeInputFile('compress_big', COMPRESS_DATA_BIG)
         TestFunctional._MakeInputFile('bl31.bin', ATF_BL31_DATA)
+        TestFunctional._MakeInputFile('bl32.bin', ATF_BL32_DATA)
         TestFunctional._MakeInputFile('scp.bin', SCP_DATA)
 
         # Add a few .dtb files for testing
@@ -3671,6 +3673,11 @@ class TestFunctional(unittest.TestCase):
         """Test that an image with an ATF BL31 binary can be created"""
         data = self._DoReadFile('169_atf_bl31.dts')
         self.assertEqual(ATF_BL31_DATA, data[:len(ATF_BL31_DATA)])
+
+    def testPackBl32(self):
+        """Test that an image with an ATF BL32 binary can be created"""
+        data = self._DoReadFile('170_atf_bl32.dts')
+        self.assertEqual(ATF_BL32_DATA, data[:len(ATF_BL32_DATA)])
 
     def testPackScp(self):
         """Test that an image with an SCP binary can be created"""

tools/binman/etype/atf_bl32.py
@@ -1,0 +1,22 @@
+# SPDX-License-Identifier: GPL-2.0+
+# Copyright (C) 2022 Texas Instruments Incorporated - https://www.ti.com/
+#
+# Entry-type module for OP-TEE Trusted OS firmware blob
+#
+
+from binman.etype.blob_named_by_arg import Entry_blob_named_by_arg
+
+class Entry_atf_bl32(Entry_blob_named_by_arg):
+    """Entry containing an OP-TEE Trusted OS (TEE) BL32 blob
+
+    Properties / Entry arguments:
+        - atf-bl32-path: Filename of file to read into entry. This is typically
+            called bl32.bin or bl32.elf
+
+    This entry holds the run-time firmware, typically started by U-Boot SPL.
+    See the U-Boot README for your architecture or board for how to use it. See
+    https://github.com/OP-TEE/optee_os for more information about OP-TEE.
+    """
+    def __init__(self, section, etype, node):
+        super().__init__(section, etype, node, 'atf-bl32')
+        self.external = True

tools/binman/test/170_atf_bl32.dts
@@ -1,0 +1,16 @@
+// SPDX-License-Identifier: GPL-2.0+
+
+/dts-v1/;
+
+/ {
+	#address-cells = <1>;
+	#size-cells = <1>;
+
+	binman {
+		size = <16>;
+
+		atf-bl32 {
+			filename = "bl32.bin";
+		};
+	};
+};

commit 91377bdb68978908ae1ee584006820f2c520e993

tools: binman: Rename atf-bl32 to tee-os
tee-os is a more appropriate name for OP-TEE binary.

Fixes: dd0bb6313c97 ("binman: Add support for TEE BL32")
Signed-off-by: Roger Quadros <rogerq@kernel.org>

Makefile
@@ -1329,9 +1329,6 @@ u-boot.ldr:	u-boot
 ifneq ($(ATF),)
 BL31 = $(ATF)
 endif
-ifneq ($(TEE),)
-BL32 = $(TEE)
-endif
 default_dt := $(if $(DEVICE_TREE),$(DEVICE_TREE),$(CONFIG_DEFAULT_DEVICE_TREE))
 quiet_cmd_binman = BINMAN  $@
 cmd_binman = $(srctree)/tools/binman/binman $(if $(BINMAN_DEBUG),-D) \
@@ -1341,7 +1338,7 @@ cmd_binman = $(srctree)/tools/binman/binman $(if $(BINMAN_DEBUG),-D) \
 		-I . -I $(srctree) -I $(srctree)/board/$(BOARDDIR) \
 		-I arch/$(ARCH)/dts -a of-list=$(CONFIG_OF_LIST) \
 		-a atf-bl31-path=${BL31} \
-		-a atf-bl32-path=${BL32} \
+		-a tee-os-path=${TEE} \
 		-a default-dt=$(default_dt) \
 		-a scp-path=$(SCP) \
 		$(BINMAN_$(@F))

tools/binman/README.entries
@@ -25,12 +25,12 @@ about ATF.
 
 
 
-Entry: atf-bl32: Entry containing an OP-TEE Trusted OS (TEE) BL32 blob
--------------------------------------------------------------------------
+Entry: tee-os: Entry containing an OP-TEE Trusted OS (TEE) blob
+---------------------------------------------------------------
 
 Properties / Entry arguments:
-    - atf-bl32-path: Filename of file to read into entry. This is typically
-        called bl32.bin or bl32.elf
+    - tee-os-path: Filename of file to read into entry. This is typically
+        called tee-pager.bin
 
 This entry holds the run-time firmware, typically started by U-Boot SPL.
 See the U-Boot README for your architecture or board for how to use it. See

tools/binman/etype/tee_os.py
@@ -6,17 +6,17 @@
 
 from binman.etype.blob_named_by_arg import Entry_blob_named_by_arg
 
-class Entry_atf_bl32(Entry_blob_named_by_arg):
-    """Entry containing an OP-TEE Trusted OS (TEE) BL32 blob
+class Entry_tee_os(Entry_blob_named_by_arg):
+    """Entry containing an OP-TEE Trusted OS (TEE) blob
 
     Properties / Entry arguments:
-        - atf-bl32-path: Filename of file to read into entry. This is typically
-            called bl32.bin or bl32.elf
+        - tee-os-path: Filename of file to read into entry. This is typically
+            called tee-pager.bin
 
     This entry holds the run-time firmware, typically started by U-Boot SPL.
     See the U-Boot README for your architecture or board for how to use it. See
     https://github.com/OP-TEE/optee_os for more information about OP-TEE.
     """
     def __init__(self, section, etype, node):
-        super().__init__(section, etype, node, 'atf-bl32')
+        super().__init__(section, etype, node, 'tee-os')
         self.external = True

tools/binman/ftest.py
@@ -76,7 +76,7 @@ FSP_M_DATA            = b'fsp_m'
 FSP_S_DATA            = b'fsp_s'
 FSP_T_DATA            = b'fsp_t'
 ATF_BL31_DATA         = b'bl31'
-ATF_BL32_DATA         = b'bl32'
+TEE_OS_DATA           = b'this is some random tee OS data'
 SCP_DATA              = b'scp'
 TEST_FDT1_DATA        = b'fdt1'
 TEST_FDT2_DATA        = b'test-fdt2'
@@ -179,7 +179,7 @@ class TestFunctional(unittest.TestCase):
         TestFunctional._MakeInputFile('compress', COMPRESS_DATA)
         TestFunctional._MakeInputFile('compress_big', COMPRESS_DATA_BIG)
         TestFunctional._MakeInputFile('bl31.bin', ATF_BL31_DATA)
-        TestFunctional._MakeInputFile('bl32.bin', ATF_BL32_DATA)
+        TestFunctional._MakeInputFile('tee-pager.bin', TEE_OS_DATA)
         TestFunctional._MakeInputFile('scp.bin', SCP_DATA)
 
         # Add a few .dtb files for testing
@@ -3674,10 +3674,10 @@ class TestFunctional(unittest.TestCase):
         data = self._DoReadFile('169_atf_bl31.dts')
         self.assertEqual(ATF_BL31_DATA, data[:len(ATF_BL31_DATA)])
 
-    def testPackBl32(self):
-        """Test that an image with an ATF BL32 binary can be created"""
-        data = self._DoReadFile('170_atf_bl32.dts')
-        self.assertEqual(ATF_BL32_DATA, data[:len(ATF_BL32_DATA)])
+    def testPackTeeOs(self):
+        """Test that an image with a TEE binary can be created"""
+        data = self._DoReadFile('188_tee_os.dts')
+        self.assertEqual(TEE_OS_DATA, data[:len(TEE_OS_DATA)])
 
     def testPackScp(self):
         """Test that an image with an SCP binary can be created"""

tools/binman/test/188_tee_os.dts
@@ -7,10 +7,8 @@
 	#size-cells = <1>;
 
 	binman {
-		size = <16>;
-
-		atf-bl32 {
-			filename = "bl32.bin";
+		tee-os {
+			filename = "tee-pager.bin";
 		};
 	};
 };