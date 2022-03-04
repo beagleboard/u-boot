03c6c015a4dfb93c7e6a362e005037261dcefcb4
Vignesh Raghavendra <vigneshr@ti.com>
2022-03-04 22:27:54 +0530

    net: am65-cpsw-nuss: Add compatible sync with upstream
    Keep compatible list in sync with upstream.
    
    Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>
    Signed-off-by: Nishanth Menon <nm@ti.com>

--- drivers/net/ti/am65-cpsw-nuss.c
+++ drivers/net/ti/am65-cpsw-nuss.c
@@ -798,7 +798,7 @@ out:
 static const struct udevice_id am65_cpsw_nuss_ids[] = {
 	{ .compatible = "ti,am654-cpsw-nuss" },
 	{ .compatible = "ti,j721e-cpsw-nuss" },
-	{ .compatible = "ti,am64-cpsw-nuss" },
+	{ .compatible = "ti,am642-cpsw-nuss" },
 	{ }
 };
 
