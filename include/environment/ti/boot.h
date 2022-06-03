beagleboard/u-boot

commit 7a7db34da08cee3141d45e694428a1f28c76219c

board: ti: am57xx-idk: Choose the right DTB based on detected LCD Panel
The board file sets the 'idk_lcd' environment variable based on
what LCD panel was detected. We just need to choose the right
DTB based on that.

Signed-off-by: Roger Quadros <rogerq@ti.com>
Signed-off-by: Amjad Ouled-Ameur <aouledameur@baylibre.com>

include/environment/ti/boot.h
@@ -209,16 +209,26 @@
 			"setenv fdtfile am57xx-beagle-x15-revc.dtb; fi;" \
 		"if test $board_name = am5729_beagleboneai; then " \
 			"setenv fdtfile am5729-beagleboneai.dtb; fi;" \
-		"if test $board_name = am572x_idk; then " \
+		"if test $board_name = am572x_idk && test $idk_lcd = no; then " \
 			"setenv fdtfile am572x-idk.dtb; fi;" \
-		"if test $board_name = am574x_idk; then " \
+		"if test $board_name = am572x_idk && test $idk_lcd = osd101t2045; then " \
+			"setenv fdtfile am572x-idk-lcd-osd101t2045.dtb; fi;" \
+		"if test $board_name = am572x_idk && test $idk_lcd = osd101t2587; then " \
+			"setenv fdtfile am572x-idk-lcd-osd101t2587.dtb; fi;" \
+		"if test $board_name = am574x_idk && test $idk_lcd = no; then " \
 			"setenv fdtfile am574x-idk.dtb; fi;" \
+		"if test $board_name = am574x_idk && test $idk_lcd = osd101t2587; then " \
+			"setenv fdtfile am574x-idk-lcd-osd101t2587.dtb; fi;" \
 		"if test $board_name = am57xx_evm; then " \
 			"setenv fdtfile am57xx-evm.dtb; fi;" \
 		"if test $board_name = am57xx_evm_reva3; then " \
 			"setenv fdtfile am57xx-evm-reva3.dtb; fi;" \
-		"if test $board_name = am571x_idk; then " \
+		"if test $board_name = am571x_idk && test $idk_lcd = no; then " \
 			"setenv fdtfile am571x-idk.dtb; fi;" \
+		"if test $board_name = am571x_idk && test $idk_lcd = osd101t2045; then " \
+			"setenv fdtfile am571x-idk-lcd-osd101t2045.dtb; fi;" \
+		"if test $board_name = am571x_idk && test $idk_lcd = osd101t2587; then " \
+			"setenv fdtfile am571x-idk-lcd-osd101t2587.dtb; fi;" \
 		"if test $fdtfile = undefined; then " \
 			"echo WARNING: Could not determine device tree to use; fi; \0"
 

commit e6e2d81da745c31d44f91b851780957a3b5f476b

env: ti: boot: fix AM5 IDK, init idk_lcd
The variable $idk_lcd is used in several "test" commands to select
which LCD pannel to select on AM57xx IDK boards, but this variable is
not set in the default environment.

It turns out that using u-boot `test` command with an unset varible
will always return true, e.g.

  => env print dummy_var
  ## Error: "dummy_var" not defined
  => if test $dummy_var = whatever; then echo true; else echo false; fi
  true

Therefore, in a default env (with no $idk_lcd set) the findfdt_iterX
checks that check $idk_lcd will *all* return true, which means the
last one wins.

Fix this by ensuring that $idk_lcd has a default value.

Tested on am571x-idk.

Fixes: LCPD-26605
Fixes: 7a7db34da08c ("board: ti: am57xx-idk: Choose the right DTB based on detected LCD Panel")
Signed-off-by: Kevin Hilman <khilman@baylibre.com>

include/environment/ti/boot.h
@@ -199,6 +199,7 @@
 #ifdef CONFIG_OMAP54XX
 
 #define DEFAULT_FDT_TI_ARGS \
+        "idk_lcd=no\0" \
 	"findfdt_iter1=" \
 		"if test $board_name = omap5_uevm; then " \
 			"setenv fdtfile omap5-uevm.dtb; fi; " \