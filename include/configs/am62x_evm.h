beagleboard/u-boot

commit be0386826c62e8dc803c8ed39d5eb27299036dd2

configs: am6*_evm/j721e_evm: prevent optargs from getting overwritten
Fix issue that saved optargs was getting overwritten when 'run args_all' was called.
This was a issue in all K3 based configurations and is resolved by prepending the
original optargs when set with args_all.

Signed-off-by: Matt Ranostay <mranostay@ti.com>

include/configs/am62x_evm.h
@@ -172,8 +172,8 @@
 		"setenv fdtfile ${name_fdt}\0"				\
 	"name_kern=Image\0"						\
 	"console=ttyS2,115200n8\0"					\
-	"args_all=setenv optargs earlycon=ns16550a,mmio32,0x02800000 "	\
-		"${mtdparts}\0"						\
+	"args_all=setenv optargs ${optargs} "				\
+		"earlycon=ns16550a,mmio32,0x02880000 ${mtdparts}\0"	\
 	"run_kern=booti ${loadaddr} ${rd_spec} ${fdtaddr}\0"
 
 /* U-Boot MMC-specific configuration */

include/configs/am64x_evm.h
@@ -114,8 +114,8 @@
 			"echo WARNING: Could not determine device tree to use; fi; \0" \
 	"name_kern=Image\0"						\
 	"console=ttyS2,115200n8\0"					\
-	"args_all=setenv optargs earlycon=ns16550a,mmio32,0x02800000 "	\
-		"${mtdparts}\0"						\
+	"args_all=setenv optargs ${optargs} "				\
+		"earlycon=ns16550a,mmio32,0x02880000 ${mtdparts}\0"	\
 	"run_kern=booti ${loadaddr} ${rd_spec} ${fdtaddr}\0"
 
 /* U-Boot MMC-specific configuration */

include/configs/am65x_evm.h
@@ -69,8 +69,8 @@
 	"name_kern=Image\0"						\
 	"console=ttyS2,115200n8\0"					\
 	"stdin=serial,usbkbd\0"						\
-	"args_all=setenv optargs earlycon=ns16550a,mmio32,0x02800000 "  \
-		"${mtdparts}\0"						\
+	"args_all=setenv optargs ${optargs} "				\
+		"earlycon=ns16550a,mmio32,0x02880000 ${mtdparts}\0"	\
 	"run_kern=booti ${loadaddr} ${rd_spec} ${fdtaddr}\0"		\
 
 /* U-Boot MMC-specific configuration */

include/configs/j721e_evm.h
@@ -87,8 +87,8 @@
 		"setenv fdtfile ${name_fdt}\0"				\
 	"name_kern=Image\0"						\
 	"console=ttyS2,115200n8\0"					\
-	"args_all=setenv optargs earlycon=ns16550a,mmio32,0x02800000 "	\
-		"${mtdparts}\0"						\
+	"args_all=setenv optargs ${optargs} "				\
+		"earlycon=ns16550a,mmio32,0x02880000 ${mtdparts}\0"	\
 	"run_kern=booti ${loadaddr} ${rd_spec} ${fdtaddr}\0"
 
 #define PARTS_DEFAULT \

include/configs/j721s2_evm.h
@@ -70,8 +70,8 @@
 		"setenv fdtfile ${name_fdt}\0"				\
 	"name_kern=Image\0"						\
 	"console=ttyS2,115200n8\0"					\
-	"args_all=setenv optargs earlycon=ns16550a,mmio32,0x02880000 "	\
-		"${mtdparts}\0"						\
+	"args_all=setenv optargs ${optargs} "				\
+		"earlycon=ns16550a,mmio32,0x02880000 ${mtdparts}\0"	\
 	"run_kern=booti ${loadaddr} ${rd_spec} ${fdtaddr}\0"
 
 #define PARTS_DEFAULT \

commit f9ba44344aa6c9150322c3def3bbea21c26d0b5a

configs: am6*_evm/j721e_evm: Fix earlycon uart MMIO address
The earlycon UART address was unintentionally modifed to uart8 from uart0
by Commit be0386826c62 ("configs: am6*_evm/j721e_evm: prevent optargs from getting overwritten")
this change caused boot to break in multiple platforms, only J7AEP is configured
to have the earlycon on uart8.

This commit reverts the earlycon MMIO address for AM62x, AM64x, AM65x
and J721E to original expected value 0x02800000 (main_uart0), this fixes the
broken boot for the above platforms.

Fixes: be0386826c62 ("configs: am6*_evm/j721e_evm: prevent optargs from getting overwritten")

Signed-off-by: Vaishnav Achath <vaishnav.a@ti.com>

include/configs/am62x_evm.h
@@ -173,7 +173,7 @@
 	"name_kern=Image\0"						\
 	"console=ttyS2,115200n8\0"					\
 	"args_all=setenv optargs ${optargs} "				\
-		"earlycon=ns16550a,mmio32,0x02880000 ${mtdparts}\0"	\
+		"earlycon=ns16550a,mmio32,0x02800000 ${mtdparts}\0"	\
 	"run_kern=booti ${loadaddr} ${rd_spec} ${fdtaddr}\0"
 
 /* U-Boot MMC-specific configuration */

include/configs/am64x_evm.h
@@ -115,7 +115,7 @@
 	"name_kern=Image\0"						\
 	"console=ttyS2,115200n8\0"					\
 	"args_all=setenv optargs ${optargs} "				\
-		"earlycon=ns16550a,mmio32,0x02880000 ${mtdparts}\0"	\
+		"earlycon=ns16550a,mmio32,0x02800000 ${mtdparts}\0"	\
 	"run_kern=booti ${loadaddr} ${rd_spec} ${fdtaddr}\0"
 
 /* U-Boot MMC-specific configuration */

include/configs/am65x_evm.h
@@ -70,7 +70,7 @@
 	"console=ttyS2,115200n8\0"					\
 	"stdin=serial,usbkbd\0"						\
 	"args_all=setenv optargs ${optargs} "				\
-		"earlycon=ns16550a,mmio32,0x02880000 ${mtdparts}\0"	\
+		"earlycon=ns16550a,mmio32,0x02800000 ${mtdparts}\0"	\
 	"run_kern=booti ${loadaddr} ${rd_spec} ${fdtaddr}\0"		\
 
 /* U-Boot MMC-specific configuration */

include/configs/j721e_evm.h
@@ -88,7 +88,7 @@
 	"name_kern=Image\0"						\
 	"console=ttyS2,115200n8\0"					\
 	"args_all=setenv optargs ${optargs} "				\
-		"earlycon=ns16550a,mmio32,0x02880000 ${mtdparts}\0"	\
+		"earlycon=ns16550a,mmio32,0x02800000 ${mtdparts}\0"	\
 	"run_kern=booti ${loadaddr} ${rd_spec} ${fdtaddr}\0"
 
 #define PARTS_DEFAULT \