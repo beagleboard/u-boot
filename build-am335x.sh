fcef48bbb9992b02e9bf82209eeaf2b5984a0bfc
Robert Nelson <robertcnelson@gmail.com>
2022-07-13 13:29:04 -0500

    build: update
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build-am335x.sh
+++ build-am335x.sh
@@ -6,30 +6,34 @@ sys=$(uname -m)
 
 DIR="$PWD"
 
-if [ "x${sys}" = "xarmv7l" ] ; then
-	if [ ! -f ./load.menuconfig ] ; then
-		echo "Developers: too make changes: [touch load.menuconfig]"
-	fi
+config="am335x_evm_defconfig"
+
+if [ ! -f ./load.menuconfig ] ; then
+	echo "Developers: too make changes: [touch load.menuconfig]"
 fi
 
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
 
 if [ "x${sys}" = "xarmv7l" ] ; then
 	if [ -f ./load.menuconfig ] ; then
-		make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- am335x_evm_defconfig
+		make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- ${config}
 		make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
 		make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- savedefconfig
-		cp -v defconfig ./configs/am335x_evm_defconfig
+		cp -v defconfig ./configs/${config}
 		make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
 	fi
 
-	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- am335x_evm_defconfig
+	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- ${config}
 	make ARCH=arm -j2 CROSS_COMPILE=arm-linux-gnueabihf-
 else
-	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- am335x_evm_defconfig
-	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
+	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- ${config}
+
+	if [ -f ./load.menuconfig ] ; then
+		make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
+	fi
+
 	make ARCH=arm -j2 CROSS_COMPILE=arm-linux-gnueabihf-
 
 	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- savedefconfig
-	cp -v defconfig ./configs/am335x_evm_defconfig
+	cp -v defconfig ./configs/${config}
 fi
