3f031bb1baad799bec97f36ca12ca74fd7c79b05
Matt Ranostay <mranostay@ti.com>
2022-07-26 01:36:08 -0700

    arm: mach-k3: am642_init: enable dcache in board_init_f
    Enable dcache for AM642 iplatforms to allow marginally faster initial
    SPL stage, and also to create page tables for board_init_r() to use
    when caches are re-enabled for loading u-boot.img
    
    Signed-off-by: Matt Ranostay <mranostay@ti.com>

--- arch/arm/mach-k3/am642_init.c
+++ arch/arm/mach-k3/am642_init.c
@@ -280,6 +280,7 @@ void board_init_f(ulong dummy)
 		if (uclass_get_device_by_driver(UCLASS_MISC, DM_GET_DRIVER(am65_cpsw_nuss), &cpswdev))
 			printf("Failed to probe am65_cpsw_nuss driver\n");
 	}
+	spl_enable_dcache();
 }
 
 u32 spl_mmc_boot_mode(const u32 boot_device)
