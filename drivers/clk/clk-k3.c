80a023ef0af573aae9f37e733c3440d6b483d40f
Hari Nagalla <hnagalla@ti.com>
2022-08-24 06:53:07 -0500

    clk: clk-k3: Add Support for J784S4 SoC
    Add support for J784S4 SoC.
    
    Signed-off-by: Hari Nagalla <hnagalla@ti.com>

--- drivers/clk/clk-k3.c
+++ drivers/clk/clk-k3.c
@@ -2,7 +2,7 @@
 /*
  * Texas Instruments K3 clock driver
  *
- * Copyright (C) 2020-2021 Texas Instruments Incorporated - http://www.ti.com/
+ * Copyright (C) 2020-2022 Texas Instruments Incorporated - http://www.ti.com/
  *	Tero Kristo <t-kristo@ti.com>
  */
 
@@ -72,6 +72,11 @@ static const struct soc_attr ti_k3_soc_clk_data[] = {
 	{
 		.family = "J721S2",
 		.data = &j721s2_clk_platdata,
+	},
+#elif CONFIG_SOC_K3_J784S4
+	{
+		.family = "J784S4",
+		.data = &j784s4_clk_platdata,
 	},
 #endif
 #ifdef CONFIG_SOC_K3_AM625
