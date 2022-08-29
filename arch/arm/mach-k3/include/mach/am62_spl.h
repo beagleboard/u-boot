7acd92a241292f98590cfe3a3e11e2f8f065bbd9
Apurva Nandan <a-nandan@ti.com>
2022-08-29 11:38:36 +0000

    arm: mack-k3: am62x: Add SPI NAND as a boot device
    AM62x-SK supports booting up from SPI NAND flash (W35N01JW).
    Add support to allow booting using MTD SPI NAND framework.
    
    Signed-off-by: Apurva Nandan <a-nandan@ti.com>

--- arch/arm/mach-k3/include/mach/am62_spl.h
+++ arch/arm/mach-k3/include/mach/am62_spl.h
@@ -8,7 +8,7 @@
 #define _ASM_ARCH_AM62_SPL_H_
 
 /* Primary BootMode devices */
-#define BOOT_DEVICE_RAM			0x00
+#define BOOT_DEVICE_SERIAL_NAND		0x00
 #define BOOT_DEVICE_OSPI		0x01
 #define BOOT_DEVICE_QSPI		0x02
 #define BOOT_DEVICE_SPI			0x03
@@ -29,8 +29,10 @@
 
 /* U-Boot used aliases */
 #define BOOT_DEVICE_ETHERNET		0x04
+#define BOOT_DEVICE_SPINAND		0x07
 #define BOOT_DEVICE_MMC2		0x08
 #define BOOT_DEVICE_MMC1		0x09
+#define BOOT_DEVICE_RAM                 0x0F
 /* Invalid */
 #define BOOT_DEVICE_MMC2_2		0x1F
 
