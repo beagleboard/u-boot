beagleboard/u-boot

commit 30457e61bc0bdfc92ba9b69c3f670cf8a5944b38

arm: k3: config_secure.mk: Add tispl.bin to secure device builds
When building for secure devices using non-buildman based image generation
the signed tispl.bin file is called tispl.bin_HS. Also build the unsigned
tispl.bin file as expected.

Signed-off-by: Andrew Davis <afd@ti.com>

arch/arm/mach-k3/config.mk
@@ -69,6 +69,7 @@ ifeq ($(CONFIG_TI_SECURE_DEVICE),y)
 SPL_ITS := u-boot-spl-k3_HS.its
 $(SPL_ITS): export IS_HS=1
 INPUTS-y	+= tispl.bin_HS
+INPUTS-y	+= tispl.bin
 else
 SPL_ITS := u-boot-spl-k3.its
 INPUTS-y	+= tispl.bin

commit 4a5c0c3199fcd08c6eeeceb44113280dfdd2774f

arm: k3: config.mk: Add missing dependencies on tispl.bin HS
When building tispl.bin for AM62x we depend on the _HS SPL and DTB files
now as we build the secure configuration by default.
Add this dependency here.

Fixes: 30457e61bc0b ("arm: k3: config_secure.mk: Add tispl.bin to secure device builds")
Reported-by: LCPD Autobuilder <lcpd_integration@list.ti.com>
Signed-off-by: Andrew Davis <afd@ti.com>
Tested-by: Vignesh Raghavendra <vigneshr@ti.com>

arch/arm/mach-k3/config.mk
@@ -70,6 +70,7 @@ SPL_ITS := u-boot-spl-k3_HS.its
 $(SPL_ITS): export IS_HS=1
 INPUTS-y	+= tispl.bin_HS
 INPUTS-y	+= tispl.bin
+tispl.bin: $(obj)/u-boot-spl-nodtb.bin_HS $(patsubst %,$(obj)/dts/%.dtb_HS,$(subst ",,$(CONFIG_SPL_OF_LIST)))
 else
 SPL_ITS := u-boot-spl-k3.its
 INPUTS-y	+= tispl.bin