beagleboard/u-boot

commit 2a54311a126d303b2fc10ee28b350da620857585

mach-k3: am625_init: Avoid HSM SRAM accesses from A53 SPL/U-Boot
Starting with v08.05.01 TIFS, HSM SRAM access is restricted to DM R5 and
SMS Cores only, A53 SPL/U-Boot won't be able to access those SRAMs.

There are three data structures in the HSM SRAM that A53 SPL/U-Boot make
use of which needs relocation:
1. Boot index info from ROM indicating whether ROM booted from primary
   vs secondary media. This is now relocated to On Chip RAM by R5 SPL
2. EXTEND BOOT INFO which indicates images loaded by ROM, this info is
   not needed by A53 SPL and thus is limited to R5 SPL usage
3. Board EEPROM data scratch pad, which is now moved to On Chip RAM

Co-developed-by: Kamlesh Gurudasani <kamlesh@ti.com>
Signed-off-by: Kamlesh Gurudasani <kamlesh@ti.com>
Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>
Acked-by: Andrew Davis <afd@ti.com>
Tested-by: Judith Mendez <jm@ti.com>

arch/arm/mach-k3/include/mach/am62_hardware.h
@@ -72,7 +72,9 @@
 
 #define ROM_ENTENDED_BOOT_DATA_INFO		0x43c3f1e0
 
-/* Use Last 2K as Scratch pad */
-#define TI_SRAM_SCRATCH_BOARD_EEPROM_START	0x43c30000
+#define K3_BOOT_PARAM_TABLE_INDEX_OCRAM		0x7000F290
+
+/* Use OCRAM as scratch pad */
+#define TI_SRAM_SCRATCH_BOARD_EEPROM_START	0x70000000
 
 #endif /* __ASM_ARCH_AM62_HARDWARE_H */

arch/arm/mach-k3/Kconfig
@@ -79,7 +79,8 @@ config SYS_K3_BOOT_PARAM_TABLE_INDEX
 	default 0x41cfdbfc if SOC_K3_J721S2
 	default 0x41cfdbfc if SOC_K3_J784S4
 	default 0x701bebfc if SOC_K3_AM642
-	default 0x43c3f290 if SOC_K3_AM625
+	default 0x43c3f290 if SOC_K3_AM625 && CPU_V7R
+	default 0x7000f290 if SOC_K3_AM625 && ARM64
 	help
 	  Address at which ROM stores the value which determines if SPL
 	  is booted up by primary boot media or secondary boot media.

arch/arm/mach-k3/am625_init.c
@@ -38,8 +38,11 @@ static struct rom_extended_boot_data bootdata __section(".data");
 static void store_boot_info_from_rom(void)
 {
 	bootindex = *(u32 *)(CONFIG_SYS_K3_BOOT_PARAM_TABLE_INDEX);
-	memcpy(&bootdata, (uintptr_t *)ROM_ENTENDED_BOOT_DATA_INFO,
-	       sizeof(struct rom_extended_boot_data));
+
+	if (IS_ENABLED(CONFIG_CPU_V7R)) {
+		memcpy(&bootdata, (uintptr_t *)ROM_ENTENDED_BOOT_DATA_INFO,
+		       sizeof(struct rom_extended_boot_data));
+	}
 }
 
 static void ctrl_mmr_unlock(void)
@@ -180,6 +183,13 @@ void board_init_f(ulong dummy)
 
 	k3_sysfw_loader(true, NULL, NULL);
 #endif
+
+	/*
+	 * Relocate boot information to OCRAM for enable next stages to
+	 * know about primary vs backup bootmode
+	 */
+	if (IS_ENABLED(CONFIG_CPU_V7R))
+		writel(bootindex, K3_BOOT_PARAM_TABLE_INDEX_OCRAM);
 
 	/*
 	 * Force probe of clk_k3 driver here to ensure basic default clock

commit 32c06b5ce3786b01c17e8e65c9538e0af4f31c5e

mach-k3: am62_hardware: Use HSM SRAM for EEPROM scratch pad for R5 SPL
During R5 SPL stage OC RAM is unavailable hence use store EEPROM data in
HSM SRAM and use OCRAM in case of A53 SPL/U-Boot (as HSM SRAM is
unusable at this stage).

This unblocks HS-SE boot on AM62

Fixes: 2a54311a12 ("mach-k3: am625_init: Avoid HSM SRAM accesses from A53 SPL/U-Boot")
Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>

arch/arm/mach-k3/include/mach/am62_hardware.h
@@ -74,7 +74,12 @@
 
 #define K3_BOOT_PARAM_TABLE_INDEX_OCRAM		0x7000F290
 
-/* Use OCRAM as scratch pad */
+#ifdef CONFIG_CPU_V7R
+/* Use HSM SRAM scratch pad for R5 SPL */
+#define TI_SRAM_SCRATCH_BOARD_EEPROM_START	0x43c30000
+#else
+/* Use OCRAM as scratch pad for A53 SPL/U-Boot */
 #define TI_SRAM_SCRATCH_BOARD_EEPROM_START	0x70000000
+#endif
 
 #endif /* __ASM_ARCH_AM62_HARDWARE_H */