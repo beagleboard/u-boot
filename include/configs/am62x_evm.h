beagleboard/u-boot

commit 767a83a972ce6f221059038ad34b7d57c1c9184c

configs: am62x: Enable config option for am62x HS EVM
Common defconfig for HS and non HS devices.

Enabled configuration needed for HS devices to boot.

Non-HS devices will continue to boot due to runtime device type detection.
If TI_SECURE_DEV_PKG is not set the build will emit warnings, for non-HS
devices these can be ignored.

Signed-off-by: Kamlesh Gurudasani <kamlesh@ti.com>

configs/am62x_evm_a53_defconfig
@@ -1,5 +1,6 @@
 CONFIG_ARM=y
 CONFIG_ARCH_K3=y
+CONFIG_TI_SECURE_DEVICE=y
 CONFIG_TI_COMMON_CMD_OPTIONS=y
 CONFIG_SPL_GPIO_SUPPORT=y
 CONFIG_SPL_LIBCOMMON_SUPPORT=y
@@ -25,8 +26,12 @@ CONFIG_SPL_SPI_SUPPORT=y
 CONFIG_DEFAULT_DEVICE_TREE="k3-am625-sk"
 CONFIG_DISTRO_DEFAULTS=y
 # CONFIG_SYS_MALLOC_CLEAR_ON_INIT is not set
+CONFIG_FIT_IMAGE_POST_PROCESS=y
 CONFIG_SPL_LOAD_FIT=y
 CONFIG_SPL_LOAD_FIT_ADDRESS=0x81000000
+CONFIG_SPL_LOAD_FIT_APPLY_OVERLAY=y
+CONFIG_SPL_FIT_IMAGE_POST_PROCESS=y
+CONFIG_BOOTCOMMAND="run findfdt; run envboot; run init_${boot}; if test ${boot_fit} -eq 1; then run get_fit_${boot}; run get_overlaystring; run run_fit; else; run get_kern_${boot}; run get_fdt_${boot}; run get_overlay_${boot}; run run_kern; fi;"
 CONFIG_BOARD_LATE_INIT=y
 CONFIG_AVB_VERIFY=y
 CONFIG_ANDROID_AB=y

configs/am62x_evm_r5_defconfig
@@ -1,5 +1,6 @@
 CONFIG_ARM=y
 CONFIG_ARCH_K3=y
+CONFIG_TI_SECURE_DEVICE=y
 CONFIG_SPL_GPIO_SUPPORT=y
 CONFIG_SPL_LIBCOMMON_SUPPORT=y
 CONFIG_SPL_LIBGENERIC_SUPPORT=y
@@ -25,6 +26,7 @@ CONFIG_SPL_SPI_SUPPORT=y
 CONFIG_DEFAULT_DEVICE_TREE="k3-am625-r5-sk"
 CONFIG_SPL_LOAD_FIT=y
 CONFIG_SPL_LOAD_FIT_ADDRESS=0x80080000
+CONFIG_SPL_LOAD_FIT_APPLY_OVERLAY=y
 CONFIG_SPL_FIT_IMAGE_POST_PROCESS=y
 # CONFIG_DISPLAY_CPUINFO is not set
 CONFIG_SPL_SYS_REPORT_STACK_F_USAGE=y

commit 5079ccd243fb38a20749e0b0d3bd96c5b7972849

configs: am62x_evm_a53: remove bootcmd from defconfig
After commit to enable HS mode android boot was broken.
So remove bootcmd from defconfig to use distrocmd from config file

Fixes: 767a83a972ce ("configs: am62x: Enable config option for am62x HS
EVM")
Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

configs/am62x_evm_a53_defconfig
@@ -32,7 +32,6 @@ CONFIG_SPL_LOAD_FIT=y
 CONFIG_SPL_LOAD_FIT_ADDRESS=0x81000000
 CONFIG_SPL_LOAD_FIT_APPLY_OVERLAY=y
 CONFIG_SPL_FIT_IMAGE_POST_PROCESS=y
-CONFIG_BOOTCOMMAND="run findfdt; run envboot; run init_${boot}; if test ${boot_fit} -eq 1; then run get_fit_${boot}; run get_overlaystring; run run_fit; else; run get_kern_${boot}; run get_fdt_${boot}; run get_overlay_${boot}; run run_kern; fi;"
 CONFIG_BOARD_LATE_INIT=y
 CONFIG_MISC_INIT_R=y
 CONFIG_SPL_BOARD_INIT=y

include/configs/am62x_evm.h
@@ -350,8 +350,11 @@
 	"bootcmd_linux=" \
 		"if test \"${android_boot}\" -eq 0; then;" \
 			"run findfdt; run envboot;" \
-			"run init_${boot}; run get_kern_${boot}; "\
-			"run get_fdt_${boot}; run get_overlay_${boot}; run run_kern;" \
+			"if test ${boot_fit} -eq 1; then;" \
+				"run get_fit_${boot}; run get_fit_${boot}; run get_overlaystring; run run_fit;"\
+			"else;" \
+				"run get_kern_${boot}; run get_fdt_${boot}; run get_overlay_${boot}; run run_kern;" \
+			"fi;" \
 		"fi\0"
 
 #define BOOTENV_DEV_NAME_LINUX(devtypeu, devtypel, instance)	\