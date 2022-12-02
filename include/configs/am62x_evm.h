beagleboard/u-boot

commit 5079ccd243fb38a20749e0b0d3bd96c5b7972849

configs: am62x_evm_a53: remove bootcmd from defconfig
After commit to enable HS mode android boot was broken.
So remove bootcmd from defconfig to use distrocmd from config file

Fixes: 767a83a972ce ("configs: am62x: Enable config option for am62x HS
EVM")
Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

include/configs/am62x_evm.h
@@ -350,8 +350,11 @@
 	"bootcmd_linux=" \
 		"if test \"${android_boot}\" -eq 0; then;" \
 			"run findfdt; run envboot;" \
-			"run init_${boot}; run get_kern_${boot}; "\
-			"run get_fdt_${boot}; run get_overlay_${boot}; run run_kern;" \
+			"if test ${boot_fit} -eq 1; then;" \
+				"run get_fit_${boot}; run get_fit_${boot}; run get_overlaystring; run run_fit;"\
+			"else;" \
+				"run get_kern_${boot}; run get_fdt_${boot}; run get_overlay_${boot}; run run_kern;" \
+			"fi;" \
 		"fi\0"
 
 #define BOOTENV_DEV_NAME_LINUX(devtypeu, devtypel, instance)	\

configs/am62x_evm_a53_defconfig
@@ -32,7 +32,6 @@ CONFIG_SPL_LOAD_FIT=y
 CONFIG_SPL_LOAD_FIT_ADDRESS=0x81000000
 CONFIG_SPL_LOAD_FIT_APPLY_OVERLAY=y
 CONFIG_SPL_FIT_IMAGE_POST_PROCESS=y
-CONFIG_BOOTCOMMAND="run findfdt; run envboot; run init_${boot}; if test ${boot_fit} -eq 1; then run get_fit_${boot}; run get_overlaystring; run run_fit; else; run get_kern_${boot}; run get_fdt_${boot}; run get_overlay_${boot}; run run_kern; fi;"
 CONFIG_BOARD_LATE_INIT=y
 CONFIG_MISC_INIT_R=y
 CONFIG_SPL_BOARD_INIT=y

commit 1349cfdea653b1fee149f1b56012839fab40868e

include: configs: am62x: add init_${boot} into distrocmd
When removing the bootcmd from the defconfigs the 'run init_${boot}' was
left out leaving us without a rootfs to boot into.

Add 'init_${boot}' back into the distro_bootcmd

Fixes: 5079ccd243fb3 ("configs: am62x_evm_a53: remove bootcmd from defconfig")
Signed-off-by: Andrew Davis <afd@ti.com>
Signed-off-by: Bryan Brattlof <bb@ti.com>

include/configs/am62x_evm.h
@@ -140,7 +140,7 @@
 #define BOOTENV_DEV_LINUX(devtypeu, devtypel, instance) \
 	"bootcmd_linux=" \
 		"if test \"${android_boot}\" -eq 0; then;" \
-			"run findfdt; run envboot;" \
+			"run findfdt; run envboot; run init_${boot};" \
 			"if test ${boot_fit} -eq 1; then;" \
 				"run get_fit_${boot}; run get_fit_${boot}; run get_overlaystring; run run_fit;"\
 			"else;" \