57ee8b0116af819c211d79e331634b5963c07501
Sjoerd Simons <sjoerd@collabora.com>
2023-04-14 11:34:44 +0530

    configs: am62: Add configs for enabling USB in U-Boot
    Add configs for enabling USB DFU in U-Boot.
    
    Signed-off-by: Sjoerd Simons <sjoerd@collabora.com>
    Signed-off-by: Ravi Gunasekaran <r-gunasekaran@ti.com>

--- include/configs/am62x_evm.h
+++ include/configs/am62x_evm.h
@@ -11,6 +11,7 @@
 
 #include <config_distro_bootcmd.h>
 #include <environment/ti/mmc.h>
+#include <environment/ti/k3_dfu.h>
 
 /* DDR Configuration */
 #define CFG_SYS_SDRAM_BASE1		0x880000000
@@ -38,9 +39,16 @@
 	DISTRO_BOOT_DEV_PXE(func) \
 	DISTRO_BOOT_DEV_DHCP(func)
 
+#define EXTRA_ENV_DFUARGS \
+	DFU_ALT_INFO_MMC \
+	DFU_ALT_INFO_EMMC \
+	DFU_ALT_INFO_RAM \
+	DFU_ALT_INFO_OSPI
+
 /* Incorporate settings into the U-Boot environment */
 #define CFG_EXTRA_ENV_SETTINGS					\
-	BOOTENV
+	BOOTENV								\
+	EXTRA_ENV_DFUARGS
 
 /* Now for the remaining common defines */
 #include <configs/ti_armv7_common.h>
