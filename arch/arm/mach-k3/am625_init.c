e56107ff97b486bfdd4802cb8a638d6ddad4b98a
Kishon Vijay Abraham I <kishon@ti.com>
2023-06-20 16:22:26 +0530

    arm: mach-k3: am625_init: Probe AM65 CPSW NUSS
    In order to support Ethernet boot on AM62x, probe AM65 CPSW NUSS driver
    in board_init_f().
    
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>
    Signed-off-by: Siddharth Vadapalli <s-vadapalli@ti.com>

--- arch/arm/mach-k3/am625_init.c
+++ arch/arm/mach-k3/am625_init.c
@@ -212,6 +212,16 @@ void board_init_f(ulong dummy)
 	if (ret)
 		panic("DRAM init failed: %d\n", ret);
 #endif
+
+	if (IS_ENABLED(CONFIG_SPL_ETH_SUPPORT) && IS_ENABLED(CONFIG_TI_AM65_CPSW_NUSS) &&
+	    spl_boot_device() == BOOT_DEVICE_ETHERNET) {
+		struct udevice *cpswdev;
+
+		if (uclass_get_device_by_driver(UCLASS_MISC, DM_DRIVER_GET(am65_cpsw_nuss),
+						&cpswdev))
+			printf("Failed to probe am65_cpsw_nuss driver\n");
+	}
+
 	spl_enable_dcache();
 }
 
