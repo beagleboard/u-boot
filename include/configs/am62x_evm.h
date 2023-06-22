c4ebd179e4defd56f134bf3ac653cf6f310fb3a8
Vignesh Raghavendra <vigneshr@ti.com>
2023-06-22 22:31:57 +0530

    board: ti: am62x: am62x.env: Move DFU args to .env
    Use .env to fetch DFU args instead of .h file, while at that drop
    redundant mmc.h inclusion
    
    Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>

--- include/configs/am62x_evm.h
+++ include/configs/am62x_evm.h
@@ -10,8 +10,6 @@
 #define __CONFIG_AM625_EVM_H
 
 #include <config_distro_bootcmd.h>
-#include <environment/ti/mmc.h>
-#include <environment/ti/k3_dfu.h>
 
 /* DDR Configuration */
 #define CFG_SYS_SDRAM_BASE1		0x880000000
@@ -39,16 +37,9 @@
 	DISTRO_BOOT_DEV_PXE(func) \
 	DISTRO_BOOT_DEV_DHCP(func)
 
-#define EXTRA_ENV_DFUARGS \
-	DFU_ALT_INFO_MMC \
-	DFU_ALT_INFO_EMMC \
-	DFU_ALT_INFO_RAM \
-	DFU_ALT_INFO_OSPI
-
 /* Incorporate settings into the U-Boot environment */
 #define CFG_EXTRA_ENV_SETTINGS					\
 	BOOTENV								\
-	EXTRA_ENV_DFUARGS
 
 /* Now for the remaining common defines */
 #include <configs/ti_armv7_common.h>
