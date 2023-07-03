beagleboard/u-boot

commit e56107ff97b486bfdd4802cb8a638d6ddad4b98a

arm: mach-k3: am625_init: Probe AM65 CPSW NUSS
In order to support Ethernet boot on AM62x, probe AM65 CPSW NUSS driver
in board_init_f().

Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>
Signed-off-by: Siddharth Vadapalli <s-vadapalli@ti.com>

arch/arm/mach-k3/am625_init.c
@@ -212,6 +212,16 @@ void board_init_f(ulong dummy)
 	if (ret)
 		panic("DRAM init failed: %d\n", ret);
 #endif
+
+	if (IS_ENABLED(CONFIG_SPL_ETH_SUPPORT) && IS_ENABLED(CONFIG_TI_AM65_CPSW_NUSS) &&
+	    spl_boot_device() == BOOT_DEVICE_ETHERNET) {
+		struct udevice *cpswdev;
+
+		if (uclass_get_device_by_driver(UCLASS_MISC, DM_DRIVER_GET(am65_cpsw_nuss),
+						&cpswdev))
+			printf("Failed to probe am65_cpsw_nuss driver\n");
+	}
+
 	spl_enable_dcache();
 }
 

commit 01e2544d22cda48cc619c965c5427fe7de8dcb31

arm: mach-k3: am625_init: Fix SPL_ETH config
The config SPL_ETH_SUPPORT is not defined. The correct config is
SPL_ETH. Fix it.

Fixes: e56107ff97b4 ("arm: mach-k3: am625_init: Probe AM65 CPSW NUSS")
Signed-off-by: Siddharth Vadapalli <s-vadapalli@ti.com>

arch/arm/mach-k3/am625_init.c
@@ -213,7 +213,7 @@ void board_init_f(ulong dummy)
 		panic("DRAM init failed: %d\n", ret);
 #endif
 
-	if (IS_ENABLED(CONFIG_SPL_ETH_SUPPORT) && IS_ENABLED(CONFIG_TI_AM65_CPSW_NUSS) &&
+	if (IS_ENABLED(CONFIG_SPL_ETH) && IS_ENABLED(CONFIG_TI_AM65_CPSW_NUSS) &&
 	    spl_boot_device() == BOOT_DEVICE_ETHERNET) {
 		struct udevice *cpswdev;
 