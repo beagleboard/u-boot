beagleboard/u-boot

commit 2d35318b4eb77dbb6ebe98c5b407659b285ae62f

board: ti: am62x: Add support to identify device name
Add macro to enable support of board name of AM62x SIP
SKEVM at bootloader.

Signed-off-by: Nitin Yadav <n-yadav@ti.com>

board/ti/am62x/evm.c
@@ -45,6 +45,7 @@ static struct gpio_desc board_det_gpios[AM62X_LPSK_BRD_DET_COUNT];
 #define board_is_am62x_skevm()  (board_ti_k3_is("AM62-SKEVM") || \
 				 board_ti_k3_is("AM62B-SKEVM"))
 #define board_is_am62x_lp_skevm()  board_ti_k3_is("AM62-LP-SKEVM")
+#define board_is_am62x_sip_skevm()  board_ti_k3_is("AM62-SIP-SKEVM")
 #define board_is_am62x_play()	board_ti_k3_is("BEAGLEPLAY-A0-")
 
 #if CONFIG_IS_ENABLED(SPLASH_SCREEN)
@@ -255,6 +256,8 @@ static void setup_board_eeprom_env(void)
 		name = "am62x_skevm";
 	else if (board_is_am62x_lp_skevm())
 		name = "am62x_lp_skevm";
+	else if (board_is_am62x_sip_skevm())
+		name = "am62x_sip_skevm";
 	else if (board_is_am62x_play())
 		name = "am62x_beagleplay";
 	else

commit 8a25b4e31851a7ab1d7b50aea30bb5d4b847253c

board: ti: am62x: Fix AM62 SIP board detection
EEPROM of AM62SIP reads AM62SIP-SKEVM. Fix the same

Fixes: 2d35318b4eb7 ("board: ti: am62x: Add support to identify device name")
Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>

board/ti/am62x/evm.c
@@ -45,7 +45,7 @@ static struct gpio_desc board_det_gpios[AM62X_LPSK_BRD_DET_COUNT];
 #define board_is_am62x_skevm()  (board_ti_k3_is("AM62-SKEVM") || \
 				 board_ti_k3_is("AM62B-SKEVM"))
 #define board_is_am62x_lp_skevm()  board_ti_k3_is("AM62-LP-SKEVM")
-#define board_is_am62x_sip_skevm()  board_ti_k3_is("AM62-SIP-SKEVM")
+#define board_is_am62x_sip_skevm()  board_ti_k3_is("AM62SIP-SKEVM")
 #define board_is_am62x_play()	board_ti_k3_is("BEAGLEPLAY-A0-")
 
 #if CONFIG_IS_ENABLED(SPLASH_SCREEN)