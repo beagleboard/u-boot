beagleboard/u-boot

commit 64c0d9e010dabdd0f9fcfd68e6e94e7ba831ea6a

board: ti: am62x: Avoid overwriting reserve mem for AM62 SIP
AM62 SIP has 512MB RAM. But the top of the RAM is reserved for TF-A and
OPTEE. U-Boot relocating there would cause overwriting of these reserved
regions. Fix this by limit U-Boot to first 256MB of RAM for all boards
with 512MB.

Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>

board/ti/am62x/evm.c
@@ -23,6 +23,8 @@
 #include <asm/gpio.h>
 #include <cpu_func.h>
 
+#include <linux/sizes.h>
+
 #include "../common/board_detect.h"
 
 DECLARE_GLOBAL_DATA_PTR;
@@ -84,6 +86,16 @@ int dram_init(void)
 int dram_init_banksize(void)
 {
 	return fdtdec_setup_memory_banksize();
+}
+
+phys_size_t get_effective_memsize(void)
+{
+	/*
+	 * Just below 512MB are TF-A and OPTEE reserve regions, thus
+	 * SPL/U-Boot RAM has to start below that. Leave 256MB space for
+	 * all reserved memories.
+	 */
+	return gd->ram_size == SZ_512M ?  SZ_256M : gd->ram_size;
 }
 
 #if defined(CONFIG_SPL_BUILD)

commit 3978119ef836d2390fc2645d56f2286aa6542e0e

board: ti: am62x: Reduce reserved memory region
For systems with 512MB DDR, reduce the top 64MB for firmwares instead of
current 256MB. This provides more useable memory for U-Boot during image
load.

Fixes: 64c0d9e010da ("board: ti: am62x: Avoid overwriting reserve mem for AM62 SIP")
Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>

board/ti/am62x/evm.c
@@ -92,10 +92,10 @@ phys_size_t get_effective_memsize(void)
 {
 	/*
 	 * Just below 512MB are TF-A and OPTEE reserve regions, thus
-	 * SPL/U-Boot RAM has to start below that. Leave 256MB space for
+	 * SPL/U-Boot RAM has to start below that. Leave 64MB space for
 	 * all reserved memories.
 	 */
-	return gd->ram_size == SZ_512M ?  SZ_256M : gd->ram_size;
+	return gd->ram_size == SZ_512M ? SZ_512M - SZ_64M  : gd->ram_size;
 }
 
 #if defined(CONFIG_SPL_BUILD)