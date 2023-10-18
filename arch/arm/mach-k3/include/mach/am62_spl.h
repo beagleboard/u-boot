beagleboard/u-boot

commit 8c7827f522b04a0089bc075f44112e3d8458e79a

arm: mack-k3: am62x: Add SPI NAND as a boot device
AM62x-SK supports booting up from SPI NAND flash (W35N01JW).
Add support to allow booting using MTD SPI NAND framework.

Signed-off-by: Apurva Nandan <a-nandan@ti.com>

arch/arm/mach-k3/include/mach/am62_spl.h
@@ -8,7 +8,7 @@
 #define _ASM_ARCH_AM62_SPL_H_
 
 /* Primary BootMode devices */
-#define BOOT_DEVICE_RAM			0x00
+#define BOOT_DEVICE_SERIAL_NAND		0x00
 #define BOOT_DEVICE_OSPI		0x01
 #define BOOT_DEVICE_QSPI		0x02
 #define BOOT_DEVICE_SPI			0x03
@@ -29,8 +29,10 @@
 
 /* U-Boot used aliases */
 #define BOOT_DEVICE_ETHERNET		0x04
+#define BOOT_DEVICE_SPINAND		0x07
 #define BOOT_DEVICE_MMC2		0x08
 #define BOOT_DEVICE_MMC1		0x09
+#define BOOT_DEVICE_RAM                 0x0F
 /* Invalid */
 #define BOOT_DEVICE_MMC2_2		0x1F
 

arch/arm/mach-k3/am625_init.c
@@ -297,6 +297,9 @@ static u32 __get_primary_bootmedia(u32 devstat)
 	case BOOT_DEVICE_EMMC:
 		return BOOT_DEVICE_MMC1;
 
+	case BOOT_DEVICE_SERIAL_NAND:
+		return BOOT_DEVICE_SPINAND;
+
 	case BOOT_DEVICE_MMC:
 		if ((bootmode_cfg & MAIN_DEVSTAT_PRIMARY_MMC_PORT_MASK) >>
 				MAIN_DEVSTAT_PRIMARY_MMC_PORT_SHIFT)

commit 5f8eb8ccc61b1b6df6dfbf62dd672ea027fb6850

mach-k3: am62x: Fix BOOT_DEVICE_SPINAND index
BOOT_DEVICE_SPINAND and BOOT_DEVICE_UART have same index of 0x7, this
leads to attempting SPINAND boot during UART boot. Fix this by
allocating unique value to BOOT_DEVICE_SPINAND. While at that move it
out of unused list as SPINAND boot is very much supported on AM62x SoCs.

Fixes: 8c7827f522b0 ("arm: mack-k3: am62x: Add SPI NAND as a boot device")
Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>

arch/arm/mach-k3/include/mach/am62_spl.h
@@ -19,6 +19,7 @@
 #define BOOT_DEVICE_UART		0x07
 #define BOOT_DEVICE_MMC			0x08
 #define BOOT_DEVICE_EMMC		0x09
+#define BOOT_DEVICE_SPINAND		0x10
 
 #define BOOT_DEVICE_USB			0x2A
 #define BOOT_DEVICE_DFU			0x0A
@@ -29,7 +30,6 @@
 
 /* U-Boot used aliases */
 #define BOOT_DEVICE_ETHERNET		0x04
-#define BOOT_DEVICE_SPINAND		0x07
 #define BOOT_DEVICE_MMC2		0x08
 #define BOOT_DEVICE_MMC1		0x09
 #define BOOT_DEVICE_RAM                 0x0F