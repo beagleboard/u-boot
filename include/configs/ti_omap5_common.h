beagleboard/u-boot

commit 1eaf714ce2218f53338cadae1cd59f9dc306d645

include: configs: Override get_fit_config to get FIT config for AM57x
The get_fit_config is introduced [1] in ti_armv7_common.h to mangle the
fdtfile name when used to select a config node from the OE made FIT image.
The ti_armv7_common.h is common for both K3 and AM57xx platforms. AM57xx
platforms' fdtfile name does not have '/' so the setexpr command fails and
boot hangs.

Override the get_fit_config in AM57x specific config header to get the FIT
config name.

[1]:472b8e71be84667201d0fb28649c3c6649ac49f8

Signed-off-by: Sinthu Raja <sinthu.raja@ti.com>

include/configs/ti_omap5_common.h
@@ -251,6 +251,7 @@
 	DEFAULT_LINUX_BOOT_ENV \
 	DEFAULT_MMC_TI_ARGS \
 	DEFAULT_FIT_TI_ARGS \
+	"get_fit_config=setenv name_fit_config conf-${fdtfile}\0" \
 	DEFAULT_COMMON_BOOT_TI_ARGS \
 	DEFAULT_FDT_TI_ARGS \
 	DFUARGS \

commit 4f20eb5e294ab55ed815ee1ebbfcff27aff5e3a5

include: configs: omap5: Drop the prefix conf- for name_fit_config
For legacy platforms the packaging of fitimage doesn't prefix the "conf-"
in the DTB config section. So, drop the conf- prefix from the fdtfile name

Fixes: 1eaf714ce2 "include: configs: Override get_fit_config to get FIT config for AM57x"
Signed-off-by: Sinthu Raja <sinthu.raja@ti.com>

include/configs/ti_omap5_common.h
@@ -276,7 +276,7 @@
 		"setenv bootfile zImage; " \
 	"fi\0" \
 	DEFAULT_FIT_TI_ARGS \
-	"get_fit_config=setenv name_fit_config conf-${fdtfile}\0" \
+	"get_fit_config=setenv name_fit_config ${fdtfile}\0" \
 	DEFAULT_COMMON_BOOT_TI_ARGS \
 	DEFAULT_FDT_TI_ARGS \
 	GET_OVERLAY_MMC_TI_ARGS \