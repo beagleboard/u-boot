2bf80af7a966a25890abb0fe68b4504ed680e833
Roger Quadros <rogerq@kernel.org>
2023-11-24 17:03:23 +0200

    configs: am64x_evm: provide NAND configuration
    Provide NAND device and NAND driver configuration for AM64x EVM.
    
    Signed-off-by: Roger Quadros <rogerq@kernel.org>

--- include/configs/am64x_evm.h
+++ include/configs/am64x_evm.h
@@ -21,4 +21,20 @@
 /* Now for the remaining common defines */
 #include <configs/ti_armv7_common.h>
 
+/* NAND Driver config */
+#define CFG_SYS_NAND_BASE		0x51000000
+
+#define CFG_SYS_NAND_ECCPOS		{ 2, 3, 4, 5, 6, 7, 8, 9, \
+					 10, 11, 12, 13, 14, 15, 16, 17, \
+					 18, 19, 20, 21, 22, 23, 24, 25, \
+					 26, 27, 28, 29, 30, 31, 32, 33, \
+					 34, 35, 36, 37, 38, 39, 40, 41, \
+					 42, 43, 44, 45, 46, 47, 48, 49, \
+					 50, 51, 52, 53, 54, 55, 56, 57, }
+
+#define CFG_SYS_NAND_ECCSIZE		512
+
+#define CFG_SYS_NAND_ECCBYTES		14
+/*-- end NAND config --*/
+
 #endif /* __CONFIG_AM642_EVM_H */
