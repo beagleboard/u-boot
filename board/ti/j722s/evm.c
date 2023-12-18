b8567f6f5a1e7d4c844f29f3b3d01f230c0f1e76
Jayesh Choudhary <j-choudhary@ti.com>
2023-12-18 21:26:31 +0530

    board: ti: Introduce basic board files for the J722S family
    Introduce the basic files needed to support the TI J722S family of SoCs.
    
    Co-developed-by: Vaishnav Achath <vaishnav.a@ti.com>
    Signed-off-by: Vaishnav Achath <vaishnav.a@ti.com>
    Signed-off-by: Jayesh Choudhary <j-choudhary@ti.com>

---
+++ board/ti/j722s/evm.c
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Board specific initialization for J722S platforms
+ *
+ * Copyright (C) 2023 Texas Instruments Incorporated - https://www.ti.com/
+ *
+ */
+
+#include <asm/arch/hardware.h>
+#include <asm/io.h>
+#include <common.h>
+#include <dm/uclass.h>
+#include <env.h>
+#include <fdt_support.h>
+#include <spl.h>
+
+#include "../common/board_detect.h"
+
+#ifdef CONFIG_TI_I2C_BOARD_DETECT
+/*
+ * Functions specific to EVM of J722S family.
+ */
+
+#define board_is_j722s()	board_ti_k3_is("J722SX-EVM")
+
+int do_board_detect(void)
+{
+	int ret;
+
+	if (board_ti_was_eeprom_read())
+		return 0;
+
+	ret = ti_i2c_eeprom_am6_get_base(CONFIG_EEPROM_BUS_ADDRESS,
+					 CONFIG_EEPROM_CHIP_ADDRESS);
+	if (ret) {
+		pr_err("Reading on-board EEPROM at 0x%02x failed %d\n",
+		       CONFIG_EEPROM_CHIP_ADDRESS, ret);
+	}
+
+	return ret;
+}
+
+int checkboard(void)
+{
+	struct ti_am6_eeprom *ep = TI_AM6_EEPROM_DATA;
+
+	if (do_board_detect())
+		/* EEPROM not populated */
+		printf("Board: %s rev %s\n", "J722SX-EVM", "E1");
+	else
+		printf("Board: %s rev %s\n", ep->name, ep->version);
+
+	return 0;
+}
+
+#endif
+
+int board_init(void)
+{
+	return 0;
+}
+
+int dram_init(void)
+{
+	return fdtdec_setup_mem_size_base();
+}
+
+int dram_init_banksize(void)
+{
+	return fdtdec_setup_memory_banksize();
+}
