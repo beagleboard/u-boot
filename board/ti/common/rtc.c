d9d06b1f663329e30b97a5d384d868846913cc25
Jared McArthur <j-mcarthur@ti.com>
2024-02-06 10:19:20 -0600

    j722s-evm: rtc.c & j722s_hardware.h: mux the WKUP_CLKOUT0 pin to LFOSC0_CLKOUT (32kHz clock)

--- board/ti/common/rtc.c
+++ board/ti/common/rtc.c
@@ -23,4 +23,13 @@ void board_rtc_init(void)
 	/* Make sure to mux up to take the SoC 32k from the crystal */
 	writel(MCU_CTRL_DEVICE_CLKOUT_LFOSC_SELECT_VAL,
 	       MCU_CTRL_DEVICE_CLKOUT_32K_CTRL);
+
+	/* unlock relevant partition for the wkup clocks */
+	writel(WKUP_CTRL_MMR_LOCK2_KICK0_UNLOCK_VAL, WKUP_CTRL_MMR_LOCK2_KICK0);
+	writel(WKUP_CTRL_MMR_LOCK2_KICK1_UNLOCK_VAL, WKUP_CTRL_MMR_LOCK2_KICK1);
+
+	/* mux the WKUP_CLKOUT0 pin to LFOSC0_CLKOUT */
+	val = readl(WKUP_CTRL_MMR_CFG0_CLKOUT_CTRL);
+	val = (val & ~(0x7)) | (WKUP_CTRL_WKUP_CLKOUT_SEL_LFOSC0_CLKOUT & 0x7);
+	writel(val, WKUP_CTRL_MMR_CFG0_CLKOUT_CTRL);
 }
