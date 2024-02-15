fc4ba1d97ec8d6e69b9ed93d7c36af2405c8aa5d
MD Danish Anwar <danishanwar@ti.com>
2024-02-15 15:19:53 +0530

    net: ti: icssg: Add support sending FDB command to update rx_flow_id
    ICSSG firmware supports FDB commands. Add support to send FDB commands
    from driver. Once rx_flow_id is obtained from dma, let firmware know that
    we are using this rx_flow_id by sending a FDB command.
    
    Signed-off-by: MD Danish Anwar <danishanwar@ti.com>

--- drivers/net/ti/icssg_prueth.h
+++ drivers/net/ti/icssg_prueth.h
@@ -70,6 +70,7 @@ struct prueth {
 	u8			pru_core_id;
 	u8			rtu_core_id;
 	u8			txpru_core_id;
+	u8			icssg_hwcmdseq;
 };
 
 /* config helpers */
@@ -81,5 +82,10 @@ int emac_set_port_state(struct prueth *prueth, enum icssg_port_state_cmd cmd);
 int icssg_queue_pop(struct prueth *prueth, u8 queue);
 void icssg_queue_push(struct prueth *prueth, int queue, u16 addr);
 u32 icssg_queue_level(struct prueth *prueth, int queue);
+
+/* FDB helpers */
+int icssg_send_fdb_msg(struct prueth *prueth, struct mgmt_cmd *cmd,
+		       struct mgmt_cmd_rsp *rsp);
+int emac_fdb_flow_id_updated(struct prueth *prueth);
 
 #endif /* __NET_TI_ICSSG_PRUETH_H */
