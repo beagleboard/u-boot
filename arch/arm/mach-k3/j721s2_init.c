beagleboard/u-boot

commit eaa18400977576e40f68cc643567995775a394c1

arm: k3: j721s2: Enable AVS
Enable AVS in config and probing of AVS node.

Signed-off-by: Udit Kumar <u-kumar1@ti.com>

arch/arm/mach-k3/j721s2_init.c
@@ -207,6 +207,13 @@ void k3_spl_init(void)
 
 	/* Output System Firmware version info */
 	k3_sysfw_print_ver();
+
+	if (IS_ENABLED(CONFIG_K3_AVS0)) {
+		ret = uclass_get_device_by_driver(UCLASS_MISC, DM_DRIVER_GET(k3_avs),
+						  &dev);
+		if (ret)
+			printf("AVS init failed: %d\n", ret);
+	}
 }
 
 bool check_rom_loaded_sysfw(void)

configs/j721s2_evm_r5_defconfig
@@ -155,6 +155,7 @@ CONFIG_K3_SYSTEM_CONTROLLER=y
 CONFIG_DM_PMIC=y
 CONFIG_PMIC_TPS65941=y
 CONFIG_DM_REGULATOR=y
+CONFIG_K3_AVS0=y
 CONFIG_SPL_DM_REGULATOR=y
 CONFIG_DM_REGULATOR_TPS65941=y
 CONFIG_REMOTEPROC_TI_K3_ARM64=y

commit 7d050099177240f6cdf8992310f2cf4941e954af

board: ti: j721s2: Move order of AVS probing
AVS driver was getting probed with base device tree, which
leads i2c of derivative board (AM68) in bad state.

Moving AVS probe after detection of right device tree.

Fixes: eaa184009775 ("arm: k3: j721s2: Enable AVS")
Reported-by: Minas Hambardzumyan <minas@ti.com>
Cc: Manorit Chawdhry <m-chawdhry@ti.com>
Cc: Vignesh Raghavendra <vigneshr@ti.com>
Cc: Praneeth Bajjuri <praneeth@ti.com>
Signed-off-by: Udit Kumar <u-kumar1@ti.com>

arch/arm/mach-k3/j721s2_init.c
@@ -208,12 +208,6 @@ void k3_spl_init(void)
 	/* Output System Firmware version info */
 	k3_sysfw_print_ver();
 
-	if (IS_ENABLED(CONFIG_K3_AVS0)) {
-		ret = uclass_get_device_by_driver(UCLASS_MISC, DM_DRIVER_GET(k3_avs),
-						  &dev);
-		if (ret)
-			printf("AVS init failed: %d\n", ret);
-	}
 }
 
 bool check_rom_loaded_sysfw(void)

board/ti/j721s2/evm.c
@@ -447,10 +447,19 @@ void do_dt_magic(void)
 #ifdef CONFIG_SPL_BUILD
 void board_init_f(ulong dummy)
 {
+	struct udevice *dev;
+	int ret;
 	k3_spl_init();
 #if defined(CONFIG_SPL_OF_LIST) && defined(CONFIG_TI_I2C_BOARD_DETECT)
 	do_dt_magic();
 #endif
 	k3_mem_init();
+
+	if (IS_ENABLED(CONFIG_K3_AVS0) && board_is_j721s2_som()) {
+		ret = uclass_get_device_by_driver(UCLASS_MISC, DM_DRIVER_GET(k3_avs),
+						  &dev);
+			if (ret)
+				printf("AVS init failed: %d\n", ret);
+	}
 }
 #endif