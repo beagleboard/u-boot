beagleboard/u-boot

commit a7e8f56abf905ee0984f93766f9065e65e215fed

board: ti: am62p: Add splash screen support
Add MMC and OSPI NOR flash as storage locations for splash screen Enable
video memory reservation and splash display by calling board specific
routine for splash screen.

Signed-off-by: Devarsh Thakkar <devarsht@ti.com>

board/ti/am62px/evm.c
@@ -13,8 +13,33 @@
 #include <env.h>
 #include <fdt_support.h>
 #include <spl.h>
+#include <video.h>
+#include <splash.h>
 
 #include "../common/rtc.c"
+
+#if CONFIG_IS_ENABLED(SPLASH_SCREEN)
+static struct splash_location default_splash_locations[] = {
+	{
+		.name = "sf",
+		.storage = SPLASH_STORAGE_SF,
+		.flags = SPLASH_STORAGE_RAW,
+		.offset = 0x700000,
+	},
+	{
+		.name		= "mmc",
+		.storage	= SPLASH_STORAGE_MMC,
+		.flags		= SPLASH_STORAGE_FS,
+		.devpart	= "1:1",
+	},
+};
+
+int splash_screen_prepare(void)
+{
+	return splash_source_load(default_splash_locations,
+				ARRAY_SIZE(default_splash_locations));
+}
+#endif
 
 int board_init(void)
 {
@@ -33,3 +58,31 @@ int dram_init_banksize(void)
 {
 	return fdtdec_setup_memory_banksize();
 }
+
+#if defined(CONFIG_SPL_BUILD)
+static int video_setup(void)
+{
+	if (CONFIG_IS_ENABLED(VIDEO)) {
+		ulong addr;
+		int ret;
+
+		addr = gd->relocaddr;
+		ret = video_reserve(&addr);
+		if (ret)
+			return ret;
+		debug("Reserving %luk for video at: %08lx\n",
+		      ((unsigned long)gd->relocaddr - addr) >> 10, addr);
+		gd->relocaddr = addr;
+	}
+
+	return 0;
+}
+
+void spl_board_init(void)
+{
+	video_setup();
+	enable_caches();
+	if (IS_ENABLED(CONFIG_SPL_SPLASH_SCREEN) && IS_ENABLED(CONFIG_SPL_BMP))
+		splash_display();
+}
+#endif

commit ed30ae315c83e1ed2db76b2f740859e5289737a1

board: ti: am62p: Fix compilation warning for implicit declaration
Fixes below warning by including cpu_funcs.h:

"board/ti/am62px/evm.c: In function `spl_board_init`:
board/ti/am62px/evm.c:83:9: warning: implicit declaration of function
`enable_caches` [-Wimplicit-function-declaration]
   83 |         enable_caches();"
      |         ^~~~~~~~~~~~~

While at it, also sort the include headers by placing video.h at the
end.

Fixes: a7e8f56abf9 ("board: ti: am62p: Add splash screen support")
Reviewed-by: Kamlesh Gurudasani <kamlesh@ti.com>
Signed-off-by: Devarsh Thakkar <devarsht@ti.com>

board/ti/am62px/evm.c
@@ -9,13 +9,13 @@
 #include <asm/arch/hardware.h>
 #include <asm/io.h>
 #include <common.h>
+#include <cpu_func.h>
 #include <dm/uclass.h>
 #include <env.h>
 #include <fdt_support.h>
 #include <spl.h>
-#include <video.h>
 #include <splash.h>
-
+#include <video.h>
 #include "../common/rtc.h"
 #include "../common/k3-ddr-init.h"
 