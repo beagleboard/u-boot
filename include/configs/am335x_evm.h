59a727e60c68c363e45db7259fe540cd4c2f954a
Robert Nelson <robertcnelson@gmail.com>
2025-04-17 16:15:53 -0500

    am335x: 00D0 rev for am335x-boneblack-revd.dtb
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- include/configs/am335x_evm.h
+++ include/configs/am335x_evm.h
@@ -182,6 +182,8 @@
 				"setenv fdtfile am335x-osd3358-sm-red.dtb; " \
 			"elif test $board_rev = OS01; then " \
 				"setenv fdtfile am335x-osd3358-sm-red-v4.dtb; " \
+			"elif test $board_rev = 00D0; then " \
+				"setenv fdtfile am335x-boneblack-revd.dtb; " \
 			"else " \
 				"setenv fdtfile am335x-boneblack.dtb; " \
 			"fi; " \
