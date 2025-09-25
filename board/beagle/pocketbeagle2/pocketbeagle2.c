a61b8ab408fcc3d4c9937c4e24bb6759b8b79afa
Robert Nelson <robertcnelson@gmail.com>
2025-09-25 17:11:40 -0500

    Revert "pb2: cleanup efi pocketbeagle2.h"
    This reverts commit e99c0c22b8c621ad9600c2e6b26e026f07ea1855.

--- board/beagle/pocketbeagle2/pocketbeagle2.c
+++ board/beagle/pocketbeagle2/pocketbeagle2.c
@@ -1,14 +1,50 @@
-// SPDX-License-Identifier: GPL-2.0+
+// SPDX-License-Identifier: GPL-2.0
 /*
- * https://www.beagleboard.org/boards/pocketbeagle-2
+ * https://beagleplay.org/
  *
- * Copyright (C) 2025 Texas Instruments Incorporated - https://www.ti.com/
- * Copyright (C) 2025 Robert Nelson, BeagleBoard.org Foundation
+ * Copyright (C) 2022-2023 Texas Instruments Incorporated - https://www.ti.com/
+ * Copyright (C) 2022-2023 Robert Nelson, BeagleBoard.org Foundation
  */
 
+#include <efi_loader.h>
+#include <cpu_func.h>
 #include <env.h>
 #include <fdt_support.h>
 #include <spl.h>
+
+#include <asm/arch/hardware.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+struct efi_fw_image fw_images[] = {
+	{
+		.image_type_id = POCKETBEAGLE2_TIBOOT3_IMAGE_GUID,
+		.fw_name = u"POCKETBEAGLE2_TIBOOT3",
+		.image_index = 1,
+	},
+	{
+		.image_type_id = POCKETBEAGLE2_SPL_IMAGE_GUID,
+		.fw_name = u"POCKETBEAGLE2_SPL",
+		.image_index = 2,
+	},
+	{
+		.image_type_id = POCKETBEAGLE2_UBOOT_IMAGE_GUID,
+		.fw_name = u"POCKETBEAGLE2_UBOOT",
+		.image_index = 3,
+	}
+};
+
+struct efi_capsule_update_info update_info = {
+	.dfu_string = "mmc 0=tiboot3.bin raw 0 2000 mmcpart 1;"
+	"tispl.bin fat 0 1;u-boot.img fat 0 1",
+	.num_images = ARRAY_SIZE(fw_images),
+	.images = fw_images,
+};
+
+int board_init(void)
+{
+	return 0;
+}
 
 int dram_init(void)
 {
@@ -20,7 +56,7 @@ int dram_init_banksize(void)
 	return fdtdec_setup_memory_banksize();
 }
 
-#if IS_ENABLED(CONFIG_BOARD_LATE_INIT)
+#ifdef CONFIG_BOARD_LATE_INIT
 int board_late_init(void)
 {
 	char fdtfile[50];
--- include/configs/pocketbeagle2.h
+++ include/configs/pocketbeagle2.h
@@ -1,12 +1,39 @@
-/* SPDX-License-Identifier: GPL-2.0+ */
+/* SPDX-License-Identifier: GPL-2.0 */
 /*
  * Configuration header file for PocketBeagle 2
  *
- * Copyright (C) 2025 Texas Instruments Incorporated - https://www.ti.com/
+ * https://www.beagleboard.org/boards/pocketbeagle-2
+ *
+ * Copyright (C) 2024 Texas Instruments Incorporated - https://www.ti.com/
  */
 
 #ifndef __CONFIG_POCKETBEAGLE2_H
 #define __CONFIG_POCKETBEAGLE2_H
+
+/**
+ * define POCKETBEAGLE2_TIBOOT3_IMAGE_GUID - firmware GUID for PocketBeagle 2
+ *                                        tiboot3.bin
+ * define POCKETBEAGLE2_SPL_IMAGE_GUID     - firmware GUID for PocketBeagle 2 SPL
+ * define POCKETBEAGLE2_UBOOT_IMAGE_GUID   - firmware GUID for PocketBeagle 2 UBOOT
+ *
+ * These GUIDs are used in capsules updates to identify the corresponding
+ * firmware object.
+ *
+ * Board developers using this as a starting reference should
+ * define their own GUIDs to ensure that firmware repositories (like
+ * LVFS) do not confuse them.
+ */
+#define POCKETBEAGLE2_TIBOOT3_IMAGE_GUID \
+	EFI_GUID(0x411c0fbc, 0xb090, 0x45bd, 0x8c, 0x48, \
+		0x44, 0xb8, 0xf7, 0xb7, 0xf8, 0xcc)
+
+#define POCKETBEAGLE2_SPL_IMAGE_GUID \
+	EFI_GUID(0x2cca8d5d, 0x3688, 0x4d52, 0x9b, 0x1d, \
+		0x73, 0x52, 0xaa, 0x47, 0xd1, 0x56)
+
+#define POCKETBEAGLE2_UBOOT_IMAGE_GUID \
+	EFI_GUID(0xf0bee454, 0x06e0, 0x4065, 0xbf, 0xa6, \
+		0xec, 0x82, 0xe1, 0xdc, 0xf3, 0x5d)
 
 /* Now for the remaining common defines */
 #include <configs/ti_armv7_common.h>
