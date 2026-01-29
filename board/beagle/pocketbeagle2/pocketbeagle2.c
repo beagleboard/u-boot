f78a2bea1b06f033007b3832f08465a51d9efcd9
Robert Nelson <robertcnelson@gmail.com>
2026-01-29 15:42:16 -0600

    pocketbeagle2: wire in TI_I2C_BOARD_DETECT
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- board/beagle/pocketbeagle2/pocketbeagle2.c
+++ board/beagle/pocketbeagle2/pocketbeagle2.c
@@ -13,15 +13,88 @@
 #include <fdt_support.h>
 #include <spl.h>
 #include <asm/arch/k3-ddr.h>
+#include <dm.h>
+#include <i2c.h>
+
+#include "../../ti/common/board_detect.h"
+
+enum {
+	EEPROM_RAM_SIZE_512MB = 0,
+	EEPROM_RAM_SIZE_1GB = 1
+};
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int do_eeprom_read(void)
+{
+	int ret;
+
+	if (board_ti_was_eeprom_read())
+		return 0;
+
+	ret = ti_i2c_eeprom_am6_get_base(CONFIG_EEPROM_BUS_ADDRESS,
+					 CONFIG_EEPROM_CHIP_ADDRESS);
+	if (ret) {
+		printf("EEPROM not available at 0x%02x, trying to read at 0x%02x\n",
+		       CONFIG_EEPROM_CHIP_ADDRESS, CONFIG_EEPROM_CHIP_ADDRESS + 1);
+		ret = ti_i2c_eeprom_am6_get_base(CONFIG_EEPROM_BUS_ADDRESS,
+						 CONFIG_EEPROM_CHIP_ADDRESS + 1);
+		if (ret)
+			pr_err("Reading on-board EEPROM at 0x%02x failed %d\n",
+			       CONFIG_EEPROM_CHIP_ADDRESS + 1, ret);
+	}
+
+	return ret;
+}
+
+static u8 pocketbeagle2_get_am62_ddr_size_default(void)
+{
+	struct ti_am6_eeprom *ep = TI_AM6_EEPROM_DATA;
+
+	if (do_eeprom_read()) {
+		/* EEPROM not populated */
+		puts("EEPROM: PB2 BLANK EEPROM 512MB)\n");
+	} else {
+		if (ep->name[0] == 0x50) {
+			printf("EEPROM: [%s]\n", ep->name);
+		}
+		/*
+		 * POCKETBEAGL2A00 (am6232 512MB)
+		 * POCKETBEAGL2A10 (am625 512MB)
+		 * POCKETBEAGL2A1I (am625 1GB)
+		 */
+		if (!strncmp(&ep->name[11], "2A1I", 4)) {
+			puts("EEPROM: EEPROM_RAM_SIZE_1GB\n");
+			return EEPROM_RAM_SIZE_1GB;
+		}
+	}
+
+	/* Default DDR size is 512MB */
+	return EEPROM_RAM_SIZE_512MB;
+}
 
 int dram_init(void)
 {
-	return fdtdec_setup_mem_size_base();
+	u8 ram_size;
+
+	if (!IS_ENABLED(CONFIG_CPU_V7R))
+		return fdtdec_setup_mem_size_base();
+
+	ram_size = pocketbeagle2_get_am62_ddr_size_default();
+
+	return 0;
 }
 
 int dram_init_banksize(void)
 {
-	return fdtdec_setup_memory_banksize();
+	u8 ram_size;
+
+	if (!IS_ENABLED(CONFIG_CPU_V7R))
+		return fdtdec_setup_memory_banksize();
+
+	ram_size = pocketbeagle2_get_am62_ddr_size_default();
+
+	return 0;
 }
 
 #if IS_ENABLED(CONFIG_BOARD_LATE_INIT)
